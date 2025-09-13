f72a5c35fd9e5dbcafc9873a7822e211aa47b023
Vyacheslav Kornienko <slava.kornienko16@gmail.com>
2025-09-13 14:06:29 +0300

    Bump version to 0.2.1 - patch release

--- mt5_grpc_proto/setup.py
+++ mt5_grpc_proto/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mt5_grpc_proto",
-    version="0.2.0",
+    version="0.2.1",
     description="Protocol Buffers and gRPC service definitions for MetaTrader 5 RPC Server",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
